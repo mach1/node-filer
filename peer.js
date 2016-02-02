04d5d57d9c172d8d32c236ee7a1a35a68ae24da0
Jaan Lambert <jaan.lambert@gmail.com>
2016-02-02 21:03:13 +0200

    Send file names in response to LIST

--- peer.js
+++ peer.js
@@ -1,21 +1,25 @@
 import net from 'net';
+import fs from 'fs';
 
 export default class Peer {
   constructor(ip, port) {
-    this.socket = net.createConnection(port, ip, this.handlePeerConnection);
     this.handleData = this.handleData.bind(this);
+    this.handlePeerConnection = this.handlePeerConnection.bind(this);
+
+    this.socket = net.createConnection(port, ip, this.handlePeerConnection);
+
     this.socket.on('data', this.handleData);
   }
 
   handleData(data) {
     console.log('Recieved data');
-    console.log('Data: ', data.toString());
 
-    this.socket.destroy();
-    console.log('Closed connection');
+    console.log('Data: ', data.toString());
+    var fileStream = fs.createWriteStream('data/in/test.txt');
+    fileStream.write(data);
   }
 
   handlePeerConnection() {
-    console.log("Connected.");
+    this.socket.write('LIST');
   }
 }
--- server.js
+++ server.js
@@ -1,18 +1,37 @@
 import net from 'net';
+import fs from 'fs';
 
 export default class PeerServer {
   constructor(port) {
+    this.handleConnection = this.handleConnection.bind(this);
+    this.handleData = this.handleData.bind(this);
+
     this.server = net.createServer(this.handleConnection);
     this.server.listen(port, '127.0.0.1');
     console.log('Listening on: ' + port);
   }
 
   handleConnection(socket) {
+    this.socket = socket;
     console.log("Peer connected from:", socket.remoteAddress);
-    socket.write('hello');
+
+
+    socket.on('data', this.handleData);
 
     socket.on('close', function() {
       console.log("Peer left");
     });
+  }
+
+  handleData(data) {
+    if (data.toString() === 'LIST') {
+      fs.readdir('data/out', function(err, files) {
+        this.socket.write(files.toString());
+      }.bind(this));
+    } else if (data.toString().indexOf('GET_FILE') === 0) {
+      this.socket.write('FILE');
+      var fileStream = fs.createReadStream('data/out/test.txt');
+      fileStream.pipe(this.socket);
+    }
   }
 }
