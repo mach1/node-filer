3501ff691feeb7268a5f68903c5542c7ef2ff793
Jaan Lambert <jaan.lambert@gmail.com>
2016-03-06 17:21:38 +0200

    Listen to file changes

--- index.js
+++ index.js
@@ -1,12 +1,6 @@
-import PeerServer from './server.js';
-import Peer from './peer.js';
+import chokidar from 'chokidar'
 
-const PORT = +process.argv[2];
-const PEER_ADDRESS = process.argv[3];
-const PEER_PORT = +process.argv[4];
+const watcher = chokidar.watch('data/in')
+const log = console.log.bind(console)
 
-const peerServer = new PeerServer(PORT);
-
-if (PEER_ADDRESS && PEER_PORT) {
-  let peer = new Peer(PEER_ADDRESS, PEER_PORT);
-}
+watcher.on('add', path => log(path))
